cd65f1224c47bfd3e4de24204696ff24f7169002
Kerem Korkmaz <kerem.493@hotmail.com>
2023-05-11 17:16:18 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DbKütüphaneEntities db = new DbKütüphaneEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblKategori.ToList();
+            return View(degerler);
+        }
+
+        public ActionResult KategoriEkle(TblKategori p)
+        {
+            db.TblKategori.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
