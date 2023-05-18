db35fb8d79cf6a40d3b22cb5d3bde3ce0bcea91d
Kerem Korkmaz <kerem.493@hotmail.com>
2023-05-18 17:55:38 +0300

    MVC Kategori ve Yazar işlemleri sayfaları bitti.

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using MvcKutuphane.Models.Entity;
@@ -16,12 +17,42 @@ namespace MvcKutuphane.Controllers
             var degerler = db.TblKategori.ToList();
             return View(degerler);
         }
-
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult KategoriEkle(TblKategori p)
         {
             db.TblKategori.Add(p);
             db.SaveChanges();
             return View();
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TblKategori.Find(id);
+            {
+                db.TblKategori.Remove(kategori);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TblKategori.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+
+        public ActionResult KategoriGuncelle(TblKategori p)
+        {
+            var ktg = db.TblKategori.Find(p.id);
+            ktg.ad = p.ad; 
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class YazarController : Controller
+    {
+        // GET: Yazar
+        DbKütüphaneEntities db = new DbKütüphaneEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblYazar.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YazarEkle()
+        {
+            return View();
+        }
+
+        public ActionResult YazarEkle(TblYazar p)
+        {
+            db.TblYazar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YazarSil(int id)
+        {
+            var yazar = db.TblYazar.Find(id);
+            db.TblYazar.Remove(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YazarGetir(int id)
+        {
+            var yzr = db.TblYazar.Find(id);
+            return View("YazarGetir",yzr);
+        }
+
+        public ActionResult YazarGuncelle(TblYazar p)
+        {
+            var yzr = db.TblYazar.Find(p.id);
+            yzr.ad = p.ad;
+            yzr.soyad = p.soyad;
+            yzr.detay = p.detay;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
